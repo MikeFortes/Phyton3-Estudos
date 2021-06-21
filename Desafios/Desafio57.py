80512e728b1f18096fe84bf5db371f4760b3bef6
Michael Fortes <kalibam.mike@gmail.com>
2021-06-21 15:55:17 -0300

    Resolvendo desafios 57 e 58

--- Desafios/Desafio57.py
+++ Desafios/Desafio57.py
@@ -3,11 +3,15 @@
 # ! Crie um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.
 
 sexo = ''
-while sexo.upper() not in {'F', 'M'}:
-    sexo = str(input('Digite seu sexo: [M/F]')).strip()
-if sexo == 'F':
-    sexo = 'Feminino'
-else:
-    sexo = 'Masculino'    
+while sexo.upper() not in {'F', 'M', 'm', 'f'}:
+    sexo = str(input('Digite seu sexo: [M/F]')).strip().upper()
+    if sexo == 'F':
+        sexo = 'Feminino'
+        break
+    elif sexo == 'M':
+        sexo = 'Masculino'
+        break
+    else:
+        print('Escolha uma opção valida: M para Masculino ou F para Feminino')    
 print('Seu sexo é {}'.format(sexo))
 print('Fim')
--- Desafios/Desafio58.py
+++ Desafios/Desafio58.py
@@ -1,3 +1,18 @@
 
 # ! Desafio 58
 # ! Melhore seu jogo do desafio 028 onde o computador vai "pensar" em um numero entre 0 e 10. Só que agora vai tentar advinhar até acertar, mostrando no final quantos palpites foram necessarios para vender.
+
+import random
+x = random.randint(0, 5)
+cont = 0
+num = 99
+while num != x:
+    print('-=-' * 20)
+    num = int(input('Hey você! De 1 a 5, qual numero estou pensando? '))
+    print('-=-' * 20)
+    cont = cont + 1
+    if num == x:
+        print('Parabens, você advinhou! Eu estava pensando no numero {}'.format(x))
+    else:
+        print('ERROU!!! Tente novamente outro numero')
+print('Você precisou de {} tentativa(s) para acertar.'.format(cont))
