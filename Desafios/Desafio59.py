047ef62ddaf506fe3f653de3a1b2999874bbf12f
Michael Fortes <kalibam.mike@gmail.com>
2021-06-23 09:31:25 -0300

    Update Desafio59.py

--- Desafios/Desafio59.py
+++ Desafios/Desafio59.py
@@ -8,20 +8,17 @@
 # ![5] Sair do programa
 # !Seu programa devera realizar a operação solicitada em cada caso
 op = ''
-print('-=-' * 2, ' ' * 14, 'DIGITE O PRIMEIRO VALOR: ', ' ' * 5, '-=-' * 2)
-x = int(input())
-print('-=-' * 2, ' ' * 14, 'DIGITE O SEGUNDO VALOR: ', ' ' * 6, '-=-' * 2)
-y = int(input())
+x = int(input('DIGITE O PRIMEIRO VALOR: '))
+y = int(input('DIGITE O SEGUNDO VALOR: '))
 while op != 5:
     print('-=-' * 20)
-    print('-=-' * 2, ' ' * 17, 'CALCULADORA', ' ' * 16, '-=-' * 2)
     print('-=-' * 2, ' ' * 14, 'ESCOLHA 1 OPERAÇÃO', ' ' * 12, '-=-' * 2)
     print('-=-' * 20)
-    print('-=-' * 2, ' ' * 2, '[1] - SOMAR', ' ' * 31, '-=-' * 2)
-    print('-=-' * 2, ' ' * 2, '[2] - MULTIPLICAR', ' ' * 25, '-=-' * 2)
-    print('-=-' * 2, ' ' * 2, '[3] - SABER + VALORES', ' ' * 21, '-=-' * 2)
-    print('-=-' * 2, ' ' * 2, '[4] - NOVOS NUMEROS', ' ' * 23, '-=-' * 2)
-    print('-=-' * 2, ' ' * 2, '[5] - SAIR', ' ' * 32, '-=-' * 2)
+    print('[1] - SOMAR')
+    print('[2] - MULTIPLICAR')
+    print('[3] - SABER O MAIOR VALORE')
+    print('[4] - NOVOS NUMEROS')
+    print('[5] - SAIR')
     print('-=-' * 20)
     op = int(input())
     if op == 1:
@@ -30,4 +27,18 @@ while op != 5:
     elif op == 2:
         r = x * y
         print('{} x {} = {}'.format(x, y, r))
+    elif op == 3:
+        if x > y:
+            maior = x
+            print('{} é o maior numero'.format(x))
+        elif y > x:
+            maior = y
+            print('{} é o maior numero'.format(y))
+        else:
+            print('Os numeros são iguais!')
+    elif op == 4:
+        x = int(input('DIGITE O NOVO PRIMEIRO VALOR: '))
+        y = int(input('DIGITE O NOVO SEGUNDO VALOR: '))
+    else:
+        print('Digite uma opção valida.')
 print('Finalizando...')
